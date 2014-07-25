9383eb79541c3d8993bbb94120f72618a6dbbcd1
Dimitar Nikolov <dimitarniknikolov@gmail.com>
2014-07-25 07:20:45 +0300

    added AndroidTemplate project for AndroidStudio

---
+++ android/AndroidProjectTemplateStudio/app/src/main/java/template/android/mentormate/com/androidprojecttemplatestudio/ui/TemplateActivity.java
@@ -0,0 +1,215 @@
+package template.android.mentormate.com.androidprojecttemplatestudio.ui;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import template.android.mentormate.com.androidprojecttemplatestudio.R;
+import template.android.mentormate.com.androidprojecttemplatestudio.util.L;
+
+
+public class TemplateActivity extends Activity {
+
+    private static final String TAG = L.makeLogTag(TemplateActivity.class);
+
+    // ---------------------------------------------------------------------------------------------
+    // Activity life cycle
+    // ---------------------------------------------------------------------------------------------
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        L.d(TAG, "onCreate");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Add support for inflating the &lt;fragment> tag.
+     *
+     * @param name
+     * @param context
+     * @param attrs
+     */
+    @Override
+    public View onCreateView(String name, Context context, AttributeSet attrs) {
+        L.d(TAG, "onCreateView");
+        return super.onCreateView(name, context, attrs);
+    }
+
+    /**
+     * Dispatch onStart() to all fragments. Ensure any created loaders are now started.
+     */
+    @Override
+    protected void onStart() {
+        super.onStart();
+        L.d(TAG, "onStart");
+    }
+
+    @Override
+    protected void onTitleChanged(CharSequence title, int color) {
+        super.onTitleChanged(title, color);
+        L.d(TAG, "onTitleChanged");
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        L.d(TAG, "onPostCreate");
+    }
+
+    /**
+     * Dispatch onResume() to fragments. Note that for better inter-operation with older versions of
+     * the platform, at the point of this call the fragments attached to the activity are
+     * <em>not</em> resumed. This means that in some cases the previous state may still be saved,
+     * not allowing fragment transactions that modify the state. To correctly interact with
+     * fragments in their proper state, you should instead override {@link #onResumeFragments()}.
+     */
+    @Override
+    protected void onResume() {
+        super.onResume();
+        L.d(TAG, "onResume");
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        L.d(TAG, "onPostResume");
+    }
+
+    @Override
+    public void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        L.d(TAG, "onAttachedToWindow");
+    }
+
+    @Override
+    public View onCreatePanelView(int featureId) {
+        L.d(TAG, "onCreatePanelView");
+        return super.onCreatePanelView(featureId);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        L.d(TAG, "onCreateOptionsMenu");
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    // ---------------------------------------------------------------------------------------------
+    // Activity is live
+    // ---------------------------------------------------------------------------------------------
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    // ---------------------------------------------------------------------------------------------
+    // END Activity is live
+    // ---------------------------------------------------------------------------------------------
+
+    /**
+     * Dispatch onPause() to fragments.
+     */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        L.d(TAG, "onPause");
+    }
+
+    /**
+     * Save all appropriate fragment state.
+     *
+     * @param outState
+     */
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        L.d(TAG, "onSaveInstanceState");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        L.d(TAG, "onStop");
+    }
+
+    /**
+     * Destroy all fragments and loaders.
+     */
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        L.d(TAG, "onDestroy");
+    }
+
+    @Override
+    public void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        L.d(TAG, "onDetachedFromWindow");
+    }
+
+    @Override
+    public void finish() {
+        super.finish();
+        L.d(TAG, "finish");
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        L.d(TAG, "onBackPressed");
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        L.d(TAG, "onConfigurationChanged");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        L.d(TAG, "onRestoreInstanceState");
+    }
+
+    /**
+     * Called on resume after home pressed
+     */
+    @Override
+    protected void onRestart() {
+        L.d(TAG, "onRestart");
+        super.onRestart();
+    }
+
+    // ---------------------------------------------------------------------------------------------
+    // Private methods
+    // ---------------------------------------------------------------------------------------------
+
+    // ---------------------------------------------------------------------------------------------
+    // Protected methods
+    // ---------------------------------------------------------------------------------------------
+
+    // ---------------------------------------------------------------------------------------------
+    // Public methods
+    // ---------------------------------------------------------------------------------------------
+
+    // ---------------------------------------------------------------------------------------------
+    // Overridden methods
+    // ---------------------------------------------------------------------------------------------
+
+    // ---------------------------------------------------------------------------------------------
+    // Interface implementations// ---------------------------------------------------------------------------------------------
+}
---
+++ android/AndroidProjectTemplateStudio/app/src/main/java/template/android/mentormate/com/androidprojecttemplatestudio/util/L.java
@@ -0,0 +1,147 @@
+package template.android.mentormate.com.androidprojecttemplatestudio.util;
+
+import android.util.Log;
+
+/**
+ * @author MentorMate
+ */
+
+/**
+ * Helper methods that make logging more consistent throughout the app.
+ */
+public class L {
+
+    private static final String LOG_PREFIX = "AppName_";
+    private static final int LOG_PREFIX_LENGTH = LOG_PREFIX.length();
+    private static final int MAX_LOG_TAG_LENGTH = 23;
+
+    /**
+     * WARNING: Don't use this when obfuscating class names with Proguard!
+     */
+    public static String makeLogTag(Class<? extends Object> cls) {
+        return makeLogTag(cls.getSimpleName());
+    }
+
+    private static String makeLogTag(String str) {
+        if (str.length() > MAX_LOG_TAG_LENGTH - LOG_PREFIX_LENGTH) {
+            return LOG_PREFIX + str.substring(0, MAX_LOG_TAG_LENGTH - LOG_PREFIX_LENGTH - 1);
+        }
+        return LOG_PREFIX + str;
+    }
+
+    /**
+     * Send a {@link #DEBUG} log message.
+     *
+     * @param tag Used to identify the source of a log message. It usually identifies the class or
+     *            activity where the log call occurs.
+     * @param msg The message you would like logged.
+     */
+    public static void d(final String tag, String message) {
+        Log.d(tag, message);
+    }
+
+    /**
+     * Send a {@link #DEBUG} log message and log the exception.
+     *
+     * @param tag Used to identify the source of a log message. It usually identifies the class or
+     *            activity where the log call occurs.
+     * @param msg The message you would like logged.
+     * @param tr  An exception to log
+     */
+    public static void d(final String tag, String message, Throwable cause) {
+        Log.d(tag, message, cause);
+    }
+
+    /**
+     * Send a {@link #VERBOSE} log message.
+     *
+     * @param tag Used to identify the source of a log message. It usually identifies the class or
+     *            activity where the log call occurs.
+     * @param msg The message you would like logged.
+     */
+    public static void v(final String tag, String message) {
+        Log.v(tag, message);
+    }
+
+    /**
+     * Send a {@link #VERBOSE} log message and log the exception.
+     *
+     * @param tag Used to identify the source of a log message. It usually identifies the class or
+     *            activity where the log call occurs.
+     * @param msg The message you would like logged.
+     * @param tr  An exception to log
+     */
+    public static void v(final String tag, String message, Throwable cause) {
+        Log.v(tag, message, cause);
+
+    }
+
+    /**
+     * Send an {@link #INFO} log message.
+     *
+     * @param tag Used to identify the source of a log message. It usually identifies the class or
+     *            activity where the log call occurs.
+     * @param msg The message you would like logged.
+     */
+    public static void i(final String tag, String message) {
+        Log.i(tag, message);
+    }
+
+    /**
+     * Send a {@link #INFO} log message and log the exception.
+     *
+     * @param tag Used to identify the source of a log message. It usually identifies the class or
+     *            activity where the log call occurs.
+     * @param msg The message you would like logged.
+     * @param tr  An exception to log
+     */
+    public static void i(final String tag, String message, Throwable cause) {
+        Log.i(tag, message, cause);
+    }
+
+    /**
+     * Send a {@link #WARN} log message.
+     *
+     * @param tag Used to identify the source of a log message. It usually identifies the class or
+     *            activity where the log call occurs.
+     * @param msg The message you would like logged.
+     */
+    public static void w(final String tag, String message) {
+        Log.w(makeLogTag(tag), message);
+    }
+
+    /**
+     * Send a {@link #WARN} log message and log the exception.
+     *
+     * @param tag Used to identify the source of a log message. It usually identifies the class or
+     *            activity where the log call occurs.
+     * @param msg The message you would like logged.
+     * @param tr  An exception to log
+     */
+    public static void w(final String tag, String message, Throwable cause) {
+        Log.w(makeLogTag(tag), message, cause);
+    }
+
+    /**
+     * Send an {@link #ERROR} log message.
+     *
+     * @param tag Used to identify the source of a log message. It usually identifies the class or
+     *            activity where the log call occurs.
+     * @param msg The message you would like logged.
+     */
+    public static void e(final String tag, String message) {
+        Log.e(makeLogTag(tag), message);
+    }
+
+    /**
+     * Send a {@link #ERROR} log message and log the exception.
+     *
+     * @param tag Used to identify the source of a log message. It usually identifies the class or
+     *            activity where the log call occurs.
+     * @param msg The message you would like logged.
+     * @param tr  An exception to log
+     */
+    public static void e(final String tag, String message, Throwable cause) {
+        Log.e(makeLogTag(tag), message, cause);
+    }
+}
